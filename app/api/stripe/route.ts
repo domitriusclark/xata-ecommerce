dc154233643bf1a90711c56380d289a76b3f0ec6
Domitrius Clark <domitriusaclark@gmail.com>
2023-04-18 02:27:51 -0400

    Testing webhook success

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -20,68 +20,64 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_API_KEY, {
 });
 
 export async function POST(req: NextRequest) {
-  const buf = await buffer(await req.json());
-
-  const signature = req.headers.get("stripe-signature");
-  let event: Stripe.Event;
-
-  if (!signature) {
-    console.log("❌ No signature found");
-
-    return new Response("No signature found", {
-      status: 400,
-    });
-  }
-
-  if (!process.env.STRIPE_WEBHOOK_SECRET) {
-    throw new Error(
-      "The STRIPE_WEBHOOK_SECRET environment variable is missing."
-    );
-  }
-
-  try {
-    event = stripe.webhooks.constructEvent(
-      buf.toString(),
-      signature,
-      process.env.STRIPE_WEBHOOK_SECRET
-    );
-  } catch (err: any) {
-    console.log(`❌ Error message: ${err.message}`);
-    return new Response(`Webhook Error: ${err.message}`, {
-      status: 400,
-    });
-  }
-
-  switch (event.type) {
-    case "product.created":
-      await handleProductCreated(event);
-      break;
-    // Add your handling for other webhook events here
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
+  console.log("Event received");
 
   NextResponse.json({ received: true });
 }
 
-async function handleProductCreated(event: Stripe.Event) {
-  const product = event.data.object as Stripe.Product;
+//   const signature = req.headers.get("stripe-signature");
+//   let event: Stripe.Event;
 
-  console.log({ product });
+//   if (!signature) {
+//     console.log("❌ No signature found");
 
-  const xata = getXataClient();
+//     return new Response("No signature found", {
+//       status: 400,
+//     });
+//   }
 
-  try {
-    // await xata.db.Product.create(product);
-    console.log(`✅ Created new product with id ${product.id}`);
-  } catch (error: any) {
-    console.log(`❌ Error creating product: ${error.message}`);
-    throw new Error(`Error creating product: ${error.message}`);
-  }
-}
+//   if (!process.env.STRIPE_WEBHOOK_SECRET) {
+//     throw new Error(
+//       "The STRIPE_WEBHOOK_SECRET environment variable is missing."
+//     );
+//   }
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+//   try {
+//     event = stripe.webhooks.constructEvent(
+//       buf.toString(),
+//       signature,
+//       process.env.STRIPE_WEBHOOK_SECRET
+//     );
+//   } catch (err: any) {
+//     console.log(`❌ Error message: ${err.message}`);
+//     return new Response(`Webhook Error: ${err.message}`, {
+//       status: 400,
+//     });
+//   }
+
+//   switch (event.type) {
+//     case "product.created":
+//       await handleProductCreated(event);
+//       break;
+//     // Add your handling for other webhook events here
+//     default:
+//       console.log(`Unhandled event type ${event.type}`);
+//   }
+
+//   NextResponse.json({ received: true });
+// }
+
+// async function handleProductCreated(event: Stripe.Event) {
+//   const product = event.data.object as Stripe.Product;
+
+//   console.log({ product });
+
+//   const xata = getXataClient();
+
+//   try {
+//     // await xata.db.Product.create(product);
+//     console.log(`✅ Created new product with id ${product.id}`);
+//   } catch (error: any) {
+//     console.log(`❌ Error creating product: ${error.message}`);
+//     throw new Error(`Error creating product: ${error.message}`);
+//   }
